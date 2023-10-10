77bc05fa191019660416914c38b76d9cba6b819b
Feliperto <fperezferraro@gmail.com>
2023-10-10 17:41:51 -0300

    Added classroom in the constructor

--- student.rb
+++ student.rb
@@ -1,11 +1,13 @@
 require './person'
 
 class Student < Person
-  def initialize(classroom, age, name = 'unknown', parent_permission: true)
-    super(name, age, parent_permission)
+  attr_accessor :classroom
+
+  def initialize(age, name = 'unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
-
+  
   def play_hooky
     puts '¯\\(ツ)/¯'
   end

35ce5b3999b56d272ba91496704b8cbbdcc10f7e
Felipe <fperezferraro@gmail.com>
2023-10-11 14:08:36 -0300

    Fixed errors as my code reviewer told me

--- student.rb
+++ student.rb
@@ -1,11 +1,16 @@
 require './person'
+require './classroom'
 
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age, name = 'unknown', parent_permission: true)
+  def initialize(_classroom, age, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
+  end
+
+  def classroom(classroom)
     @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
